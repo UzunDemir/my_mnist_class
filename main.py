929d63c0c983f125ec21792fe0303d8fc5a37b0a
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2024-06-21 19:35:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,73 +4,72 @@ import tensorflow as tf
 from PIL import Image
 import matplotlib.pyplot as plt
 
-# Load the pre-trained model
+# Загрузка предобученной модели
 model = tf.keras.models.load_model('mnist_cnn_model.h5')
 
-# Function to preprocess the uploaded image
+# Функция для предобработки загруженного изображения
 def preprocess_image(image):
     img = Image.open(image)
-    img = img.convert('L')  # Convert to grayscale
-    img = img.resize((28, 28))  # Resize to 28x28
-    img_array = np.array(img) / 255.0  # Convert to numpy array and normalize
-    img_array = img_array.reshape((1, 28, 28, 1))  # Reshape for CNN model
+    img = img.convert('L')  # Преобразование в grayscale
+    img = img.resize((28, 28))  # Изменение размера до 28x28
+    img_array = np.array(img) / 255.0  # Преобразование в массив и нормализация
+    img_array = img_array.reshape((1, 28, 28, 1))  # Изменение формы для модели CNN
     return img, img_array
 
-# Streamlit App
-st.title('MNIST Digit Classifier')
+# Streamlit приложение
+st.title('Классификатор рукописных цифр MNIST')
 
-uploaded_image = st.file_uploader("Upload a digit image (MNIST format)", type=["jpg", "jpeg", "png"])
+uploaded_image = st.file_uploader("Загрузите изображение цифры (формат MNIST)", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
     try:
-        # Preprocess the uploaded image
+        # Предобработка загруженного изображения
         user_image, img_array = preprocess_image(uploaded_image)
 
-        # Display the processed images
+        # Отображение обработанных изображений с помощью Matplotlib
         fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(10, 6))
 
-        # Hide the ticks
+        # Скрытие меток осей для всех подграфиков
         for axis in ax.flat:
             axis.set_xticks([])
             axis.set_yticks([])
 
-        # Original Image
-        ax[0, 0].set_title("Original Image")
-        ax[0, 0].imshow(user_image)
+        # Исходное изображение
+        ax[0, 0].set_title("Исходное изображение")
+        ax[0, 0].imshow(user_image, cmap='gray')  # Используем cmap='gray' для grayscale изображений
 
-        # Resized Image (28 * 28)
+        # Измененное изображение (28 * 28)
         resized_image = user_image.resize((28, 28))
-        ax[0, 1].set_title("Resized Image")
-        ax[0, 1].imshow(resized_image)
+        ax[0, 1].set_title("Измененное изображение")
+        ax[0, 1].imshow(resized_image, cmap='gray')
 
-        # Grayscale Image
+        # Grayscale изображение
         grayscaled_image = resized_image.convert("L")
-        ax[0, 2].set_title("Grayscaled Image")
+        ax[0, 2].set_title("Grayscale изображение")
         ax[0, 2].imshow(grayscaled_image, cmap="gray")
 
-        # Invert the image (so the text is white, and background is black)
+        # Инвертированное изображение (текст белый, фон черный)
         inverted_image = 255 - np.array(grayscaled_image)
-        ax[1, 0].set_title("Inverted Image")
+        ax[1, 0].set_title("Инвертированное изображение")
         ax[1, 0].imshow(inverted_image, cmap="gray")
 
-        # Normalize the image (divided by 255 to make them between 0 - 1)
+        # Нормализованное изображение (делим на 255, чтобы значения были от 0 до 1)
         normalized_image = inverted_image / 255.0
-        ax[1, 1].set_title("Normalized Image")
+        ax[1, 1].set_title("Нормализованное изображение")
         ax[1, 1].imshow(normalized_image, cmap="gray")
 
-        # Reshaped image
+        # Измененное форма изображение
         reshaped_image = normalized_image.reshape((28, 28))
-        ax[1, 2].set_title("Reshaped Image")
+        ax[1, 2].set_title("Измененная форма изображения")
         ax[1, 2].imshow(reshaped_image, cmap="gray")
 
         st.pyplot(fig)
 
-        # Make a prediction using the pre-trained model
-        result = model.predict(img_array) #
+        # Предсказание с использованием предобученной модели
+        result = model.predict(img_array)
         predicted_class = np.argmax(result)
 
-        st.success(f'Predicted Digit: {predicted_class}')
-        
-        
+        st.success(f'Предсказанная цифра: {predicted_class}')
+
     except Exception as e:
-        st.error(f'Error: {e}')
+        st.error(f'Ошибка: {e}')
