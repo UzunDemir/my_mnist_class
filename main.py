3d2be798125accaa96145716f2d9c77bc17e14f4
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2024-06-21 18:23:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from PIL import Image
-import io
 
 # Load the pre-trained CNN model for MNIST digits
 model = load_model('mnist_cnn_model.h5')
